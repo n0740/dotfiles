2f2bbc63d2cea4a418d56942b6409d4e71814aa7
Konstantin Bakulin <mail@kbakulin.com>
2019-09-10 12:55:14 +0200

    Add Vim Plug install script

--- install.sh
+++ install.sh
@@ -44,6 +44,7 @@ done
 
 
 echo "Installing Vim plugins"
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+mkdir -p ~/.vim/bundle/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
